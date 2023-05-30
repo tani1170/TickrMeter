b30463726ab1f60cac7ba158167b185b250f254e
Your Name <you@example.com>
2023-05-30 17:14:45 +0200

    rettelser på src

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,36 @@ export default defineConfig({
     outDir: resolve("dist"),
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "src/index.html"),
+        main: resolve(
+          __dirname,
+          "src/index.html"
+        ),
+        bestil: resolve(
+          __dirname,
+          "src/bestil.html"
+        ),
+
+        support: resolve(
+          __dirname,
+          "src/support.html"
+        ),
+        guide: resolve(
+          __dirname,
+          "src/guide.html"
+        ),
+        omos: resolve(
+          __dirname,
+          "src/omos.html"
+        ),
+        login: resolve(
+          __dirname,
+          "src/login.html"
+        ),
+        nylogin: resolve(
+          __dirname,
+          "src/nylogin.html"
+        ),
+
         // about: resolve(__dirname, "src/about.html"),
       },
     },
