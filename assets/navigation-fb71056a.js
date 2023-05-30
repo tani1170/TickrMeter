60dfac44861074fd0374758f0986e1b0a5942aef
Your Name <you@example.com>
2023-05-30 17:04:42 +0200

    Updates

---
+++ assets/navigation-fb71056a.js
@@ -0,0 +1,1 @@
+import"./main-c23491ae.js";document.getElementById("togglenav").addEventListener("click",function(){document.getElementById("nav").classList.toggle("-right-full"),document.getElementById("nav").classList.toggle("right-0")});
